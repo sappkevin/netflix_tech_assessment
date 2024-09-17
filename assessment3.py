626e676fccea53250d3fcf71c798eefc1da92d20
sappkevin <109696421+sappkevin@users.noreply.github.com>
2024-09-17 10:00:20 -0500

    changed output log name for assessment 3

--- assessment3.py
+++ assessment3.py
@@ -6,7 +6,7 @@ from googleapiclient.http import MediaIoBaseDownload, MediaIoBaseUpload
 import io
 
 # Set up logging
-logging.basicConfig(filename='./reports/assessment3.log', level=logging.INFO, 
+logging.basicConfig(filename='./reports/assessment3_report.log', level=logging.INFO, 
                     format='%(asctime)s,%(message)s', 
                     datefmt='%Y-%m-%d %H:%M:%S')
 
